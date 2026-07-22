d2650a98b1888806fc39e5d609adbf5ce523cfa9
Mohammad Rayyan Adhoni <mohammad.adhoni.2023@uni.strath.ac.uk>
2026-07-22 23:03:04 +0530

    some more changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const checkDeplyment = process.env.NETLIFY === 'true' ? true : false;
+
 export default defineConfig({
   plugins: [react()],
-  base: './', // for Github pages 
+  base: checkDeplyment ? '/' : '/devbyrayyan/',
 });
